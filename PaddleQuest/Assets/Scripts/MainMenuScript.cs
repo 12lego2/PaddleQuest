be5cadf3ee30887430285a26daedc90dfaa9929c
Jonathan Escalera Ibarra <jonathanescaleraibarra@gmail.com>
2024-08-08 23:47:41 -0700

    More UI and Button things

--- PaddleQuest/Assets/Scripts/MainMenuScript.cs
+++ PaddleQuest/Assets/Scripts/MainMenuScript.cs
@@ -2,9 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public Image button;
+    public Sprite buttonP;
+    public GameObject text;
+    //public Text text;
+
+    public void buttonPressed()
+    {
+        //add in code to make text move down with button press, for now just turning off text works
+       
+        text.SetActive(false);
+        button.sprite = buttonP;
+    }
     
     public void PlayGame()
     {
